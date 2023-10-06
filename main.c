6f6591b6d7dfbbd9a83d607c001f5d5a683bce97
Charles-Antoine <charto.bta@gmail.com>
2023-10-06 09:00:20 +0200

    v

--- main.c
+++ main.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
-struct list{
+struct StructureX{
     int value;
-    int pointer;
+    struct StructureX * pointeurX;
 };
+struct StructureX tab[];
+
+
 
 int main() {
-    struct list list1;
-    struct list list2;
-    list2.pointer = 0;
-    list1.value = 0;
-    list2.value = 1;
-    struct list * pList = &list2;
-    list1.pointer = (int) pList;
-    printf("liste 2 %d\n",list2);
-    printf("pointeur vers list2 : %d",list1.pointer);
+    for (int i = 0; i < 10; ++i) {
+        tab[i].value = i*3;
+        printf("i =%d, value =%d\n",i,tab[i].value);
+        tab[i].pointeurX = &tab[i+1];
+    }
+    printf("here: ");
+    int rang;
+    scanf("%d",&rang);
+
+    printf("Valeur de rang:%d : %d\n",rang, tab[rang].value);
+    printf("Valeur de rang:%d à travers le pointeur : %d\n",rang, tab[rang-1].pointeurX->value);
+
     return 0;
 }
